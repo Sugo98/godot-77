1b469432c49afbcc7c2f144aa2d6f3a112a8112c
SalaA98 <alesaladue@gmail.com>
2025-01-11 15:33:37 +0100

    Minor adjustments

--- game_logic/gui_enemy_test.gd
+++ game_logic/gui_enemy_test.gd
@@ -6,7 +6,7 @@ var faces_loaded = [ ]
 func _ready():
 	for i in dice_slots:
 		var slot = DiceSlot.new()
-		slot.init(FaceData.Type.ANY, Vector2i(64, 64))
+		slot.init(FaceData.Type.SWORD, Vector2i(64, 64))
 		%EnemySlots.add_child(slot)
 	
 	for i in faces_loaded.size():
